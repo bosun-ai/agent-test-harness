ce05be05812ec0082e699304a39f50cd46d138a7
Tinco Andringa <mail@tinco.nl>
2024-11-26 17:44:28 +0100

    add some more environment for fluyt

---
+++ run_fluyt_dependencies.sh
@@ -0,0 +1,7 @@
+# Start redis
+docker stop agent-test-harness-redis || true
+docker run -d --rm --name agent-test-harness-redis -p 6379:6379 redis:7-alpine 
+
+# Start qdrant
+docker stop agent-test-harness-qdrant || true
+docker run -d --rm --name agent-test-harness-qdrant -p 6333:6333 qdrant/qdrant:v1.9.2
