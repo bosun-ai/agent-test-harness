a8e209a8ad568bcd1fdb73ca563c0ed8aa69495b
Tinco Andringa <mail@tinco.nl>
2024-11-28 21:22:53 +0000

    fix connection to qdrant

--- run_fluyt_dependencies.sh
+++ run_fluyt_dependencies.sh
@@ -4,4 +4,4 @@ docker run -d --rm --name agent-test-harness-redis -p 6379:6379 redis:7-alpine
 
 # Start qdrant
 docker stop agent-test-harness-qdrant || true
-docker run -d --rm --name agent-test-harness-qdrant -p 6333:6333 qdrant/qdrant:v1.9.2
+docker run -d --rm --name agent-test-harness-qdrant -p 6333:6333 -p 6334:6334 qdrant/qdrant:v1.9.2
