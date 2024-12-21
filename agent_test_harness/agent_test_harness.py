d1984b9150bb3c1c193bfb42cd16b085d9b6a4b4
Tinco Andringa <mail@tinco.nl>
2024-12-21 11:04:29 +0100

    fix benchmark format

--- agent_test_harness/agent_test_harness.py
+++ agent_test_harness/agent_test_harness.py
@@ -21,50 +21,36 @@ class AgentTestHarness:
     def benchmark_agents(self):
         while next_run := self.benchmark.next_run():
             try:
-                results = self.benchmark_agent(next_run["agent"], next_run["repository"], next_run["iteration"])
+                results = self.benchmark_agent(next_run["run_name"],next_run["agent"], next_run["repository"])
                 self.benchmark.add_result(next_run["run_name"], results)
             except Exception as e:
                 logging.error(f"Error benchmarking agent {next_run['agent']['name']} on repository {next_run['repository']['name']}: {e}")
                 backtrace = traceback.format_exc()
                 self.benchmark.add_result(next_run["run_name"], {"error": str(e), "backtrace": backtrace})
 
-        return self.benchmark.results
+        return self.benchmark.results.values()
 
-    def benchmark_agent(self, agent: dict, repositories: list[dict], runs: int = 1):
-        benchmark_results = {
-            "agent_name": agent["name"],
-            "results": []
-        }
-        for repository in repositories:
-            benchmark_results["results"].extend(self.benchmark_agent_on_repository(agent, repository))
-        return benchmark_results
-
-    def benchmark_agent_on_repository(self, agent: dict, repository: dict):
-        repository_name = repository["name"]
-        name = f"{agent['name']}-{repository_name}"
-
+    def benchmark_agent(self, run_name: str, agent: dict, repository: dict):
         setup_script = repository["setup_script"] + "\n# Agent setup script:\n\n" + agent["setup_script"]
 
-        workspace_provider = WorkspaceProvider(name, repository, setup_script)
+        logging.info(f"Initializing workspace provider for run {run_name}...")
+
+        workspace_provider = WorkspaceProvider(run_name, repository, setup_script)
         workspace_provider.run()
 
-        logging.info(f"Running {self.runs} runs for agent {agent['name']} on repository {repository_name}...")
-        benchmark_results = []
-        for iteration in range(self.runs):
-            run_name = f"{name}-{iteration}"
-            benchmark_result = {
-                "repository_url": repository["url"],
-                "results": {},
-                "run": run_name
-            }
+        benchmark_result = {
+            "agent_name": agent["name"],
+            "repository_url": repository["url"],
+            "result": {},
+            "run": run_name
+        }
 
-            logging.info(f"Initializing agent test benchmark for run {run_name}...")
-            agent_test_benchmark = AgentTestBenchmark(run_name, self.llm_proxy, workspace_provider, agent, repository)
+        logging.info(f"Initializing agent test benchmark for run {run_name}...")
+        agent_test_benchmark = AgentTestBenchmark(run_name, self.llm_proxy, workspace_provider, agent, repository)
 
-            logging.info(f"Running agent test benchmark for run {run_name}...")
-            benchmark_result["results"] = agent_test_benchmark.run()
-            benchmark_results.append(benchmark_result)
+        logging.info(f"Running agent test benchmark for run {run_name}...")
+        benchmark_result["result"] = agent_test_benchmark.run()
 
         workspace_provider.stop()
 
-        return benchmark_results
+        return benchmark_result
